e1323f678a7e9ba222d5cd153e51235ddbdabdd4
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2024-07-31 17:03:18 +0300

    CategoryController.cs and migrations added

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    // GET
+    public IActionResult Index()
+    {
+        var objCategoryList = _db.Categories.ToList();
+        return View();
+    }
+}

3318cdf50da6b4e09e26a232818701c6aacbb4a8
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2024-08-03 21:32:47 +0300

    App Tested

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0341d25d58ad79ad82caf238ad207c0905e22e5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ee0c1bf3ece5e13e0dcebb6b9d245f5cc2bd9ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

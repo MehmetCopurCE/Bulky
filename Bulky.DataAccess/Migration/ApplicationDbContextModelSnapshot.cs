4ba5dc1a031c23335e22c8741ca3696bf2a9068c
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2024-08-07 20:37:38 +0300

    Create and Updated combined as Upsert and Richtext added and tested successfully!

--- Bulky.DataAccess/Migration/ApplicationDbContextModelSnapshot.cs
+++ Bulky.DataAccess/Migration/ApplicationDbContextModelSnapshot.cs
@@ -74,11 +74,18 @@ namespace Bulky.DataAccess.Migration
                         .IsRequired()
                         .HasColumnType("longtext");
 
+                    b.Property<int>("CategoryId")
+                        .HasColumnType("int");
+
                     b.Property<string>("Description")
                         .IsRequired()
                         .HasColumnType("longtext");
 
                     b.Property<string>("ISBN")
+                        .IsRequired()
+                        .HasColumnType("longtext");
+
+                    b.Property<string>("ImageUrl")
                         .IsRequired()
                         .HasColumnType("longtext");
 
@@ -100,6 +107,8 @@ namespace Bulky.DataAccess.Migration
 
                     b.HasKey("Id");
 
+                    b.HasIndex("CategoryId");
+
                     b.ToTable("Products");
 
                     b.HasData(
@@ -107,8 +116,10 @@ namespace Bulky.DataAccess.Migration
                         {
                             Id = 1,
                             Author = "Billy Spark",
+                            CategoryId = 1,
                             Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
                             ISBN = "SWD9999001",
+                            ImageUrl = "https://via.placeholder.com/150",
                             ListPrice = 99.0,
                             Price = 90.0,
                             Price100 = 80.0,
@@ -119,8 +130,10 @@ namespace Bulky.DataAccess.Migration
                         {
                             Id = 2,
                             Author = "Nancy Hoover",
+                            CategoryId = 2,
                             Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
                             ISBN = "CAW777777701",
+                            ImageUrl = "https://via.placeholder.com/150",
                             ListPrice = 40.0,
                             Price = 30.0,
                             Price100 = 20.0,
@@ -131,8 +144,10 @@ namespace Bulky.DataAccess.Migration
                         {
                             Id = 3,
                             Author = "Julian Button",
+                            CategoryId = 3,
                             Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
                             ISBN = "RITO5555501",
+                            ImageUrl = "https://via.placeholder.com/150",
                             ListPrice = 55.0,
                             Price = 50.0,
                             Price100 = 35.0,
@@ -143,8 +158,10 @@ namespace Bulky.DataAccess.Migration
                         {
                             Id = 4,
                             Author = "Abby Muscles",
+                            CategoryId = 2,
                             Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
                             ISBN = "WS3333333301",
+                            ImageUrl = "https://via.placeholder.com/150",
                             ListPrice = 70.0,
                             Price = 65.0,
                             Price100 = 55.0,
@@ -155,8 +172,10 @@ namespace Bulky.DataAccess.Migration
                         {
                             Id = 5,
                             Author = "Ron Parker",
+                            CategoryId = 1,
                             Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
                             ISBN = "SOTJ1111111101",
+                            ImageUrl = "https://via.placeholder.com/150",
                             ListPrice = 30.0,
                             Price = 27.0,
                             Price100 = 20.0,
@@ -167,14 +186,27 @@ namespace Bulky.DataAccess.Migration
                         {
                             Id = 6,
                             Author = "Laura Phantom",
+                            CategoryId = 3,
                             Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
                             ISBN = "FOT000000001",
+                            ImageUrl = "https://via.placeholder.com/150",
                             ListPrice = 25.0,
                             Price = 23.0,
                             Price100 = 20.0,
                             Price50 = 22.0,
                             Title = "Leaves and Wonders"
                         });
+                });
+
+            modelBuilder.Entity("Bulky.Models.Product", b =>
+                {
+                    b.HasOne("Bulky.Models.Category", "Category")
+                        .WithMany()
+                        .HasForeignKey("CategoryId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Category");
                 });
 #pragma warning restore 612, 618
         }
--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,9 @@
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Bulky.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace BulkyWeb.Areas.Admin.Controllers;
 
 [Area("Admin")]
@@ -18,25 +21,63 @@ public class ProductController : Controller
         return View(productList);
     }
     
-    public IActionResult Create()
+    public IActionResult Upsert(int? id)  //create => Update - Insert = Upsert
     {
-        return View();
+        // IEnumerable<SelectListItem> CategoryList = _unitOfWork._categoryRepository.
+        //     GetAll().Select(u => new SelectListItem
+        //     {
+        //         Text = u.Name,
+        //         Value = u.Id.ToString()
+        //     });
+        
+        //ViewBag.CategoryList = CategoryList;
+        //ViewData["CategoryList"] = CategoryList;
+
+        ProductVM productVm = new()
+        {
+            CategoryList = _unitOfWork._categoryRepository.GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            }),
+            Product = new Product()
+        };
+        if(id == null || id == 0)
+        {
+            //create
+            return View(productVm);
+        }
+        else
+        {
+            //update
+            productVm.Product = _unitOfWork._productRepository.Get(u => u.Id == id);
+            return View(productVm);
+
+        }
     }
     
     [HttpPost]
-    public IActionResult Create(Product product)
+    public IActionResult Upsert(ProductVM productVm, IFormFile? file)
     {
         if (ModelState.IsValid)
         {
-            _unitOfWork._productRepository.Add(product);
+            _unitOfWork._productRepository.Add(productVm.Product);
             _unitOfWork.Save();
             TempData["success"] = "Product created successfully";
             return RedirectToAction("Index","Product");  
         }
-        return View();  
+        else
+        {
+            productVm.CategoryList = _unitOfWork._categoryRepository.GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            return View(productVm);
+        }
     }
     
-    public IActionResult Edit(int? id)
+    /*public IActionResult Edit(int? id)
     {
         if (id == null || id == 0)
         {
@@ -61,7 +102,7 @@ public class ProductController : Controller
             return RedirectToAction("Index","Product");  
         }
         return View();  
-    }
+    }*/
     
     public IActionResult Delete(int? id)
     {
